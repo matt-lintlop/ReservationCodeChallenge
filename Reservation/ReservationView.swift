376561692fc0a3733692c2da74ee0e988a1738e2
Matt Lintlop <matt_lintlop@icloud.com>
2017-11-13 14:11:52 -0800

    Implemented outlets for Reservation View using view tags.

--- Reservation/ReservationView.swift
+++ Reservation/ReservationView.swift
@@ -10,12 +10,37 @@ import UIKit
 
 class ReservationView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+//    weak var dateLabel: UILabel!
+//    weak var serviveTypeLabel: UILabel!
+//    weak var durationLabel: UILabel!
+//    weak var descriptionLabel: UILabel!
+ 
+    weak var dateLabel: UIView!
+    weak var serviveTypeLabel: UIView!
+    weak var durationLabel: UIView!
+    weak var descriptionLabel: UIView!
+
+    weak var rescheduleButtonBackground: UIView!
+    weak var cancelButtonBackground: UIView!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+//        dateLabel = self.viewWithTag(1)! as! UILabel
+//        serviveTypeLabel = self.viewWithTag(2)! as! UILabel
+//        durationLabel = self.viewWithTag(3)! as! UILabel
+//        descriptionLabel = self.viewWithTag(4)! as! UILabel
+
+        dateLabel = self.viewWithTag(1)!
+        serviveTypeLabel = self.viewWithTag(2)!
+        durationLabel = self.viewWithTag(3)!
+        descriptionLabel = self.viewWithTag(4)!
+
+        rescheduleButtonBackground = self.viewWithTag(5)!
+        rescheduleButtonBackground.layer.cornerRadius = 10
+
+        cancelButtonBackground = self.viewWithTag(6)!
+        cancelButtonBackground.layer.cornerRadius = 10
     }
-    */
 
 }

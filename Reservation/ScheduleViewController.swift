68c81dfb1c7027b6709369b32ec8dbfe55bf7ee7
Matt Lintlop <matt_lintlop@icloud.com>
2017-11-15 07:51:26 -0800

    Started to implement the Reserve button in the Schedule view controller.

--- Reservation/ScheduleViewController.swift
+++ Reservation/ScheduleViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ScheduleViewController.swift
 //  Reservation
 //
 //  Created by Matthew Lintlop on 11/12/17.
@@ -10,12 +10,22 @@ import UIKit
 
 class ScheduleViewController: UIViewController {
 
+    @IBOutlet weak var reserveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+ 
+        reserveButton.setBackgroundImage(UIImage(named: "/Images/Button.png"), for: .normal)
+        reserveButton.setBackgroundImage(UIImage(named: "/Images/ButtonHighlighted.png"), for: .highlighted)
+        reserveButton.setNeedsDisplay()        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func reservePressed(_ sender: Any) {
+        print("Reserve Button Pressed")
+    }
 }
+
